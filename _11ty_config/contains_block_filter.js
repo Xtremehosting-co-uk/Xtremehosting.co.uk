1b7898a3efb5d93e25d8bf7b61b46f618764a059
WARPED IBUN <84038557+matty5350@users.noreply.github.com>
2024-01-30 21:13:33 +0000

    Revert " commit"
    This reverts commit 254ed97fbc591f536070c053071527efb5fe3b5d.

--- _11ty_config/contains_block_filter.js
+++
@@ -1,6 +0,0 @@
-module.exports = (content_blocks, blockName) => {
-    if (!Array.isArray(content_blocks)) {
-      return false;
-    }
-    return content_blocks.some(block => block._bookshop_name === blockName);
-}
--- component-library/bookshop/ymlify.js
+++
@@ -1,7 +0,0 @@
-import yaml from "js-yaml";
-
-module.exports = function (Liquid) {
-    this.registerFilter("ymlify", (yml) => {
-        return yaml.load(yml)
-    });
-}
